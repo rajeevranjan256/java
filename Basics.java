375720c9189c99097e8b6423a517f8546404332b
rajeevranjan256 <rajeevranjan12346789@gmail.com>
2025-08-17 21:54:37 +0530

    adda somthing

--- Basics.java
+++ Basics.java
@@ -2,6 +2,8 @@ public class Basics {
     public static void main(String args[]){
         System.out.println("Hello World");
         System.out.println("1,2,3,4,5,6");
+        System.out.println("Hello World");
+        System.out.println("1,2,3,4,5,6");
     }
     
 }
